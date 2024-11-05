65eb3c7cb13b92effa7ff282c07e900e6ed3b790
mpoelzl <mpoelzl@redeaglesolutions.com>
2024-11-05 15:47:08 +0100

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ namespace mintspark {
      * NeZha V2
      */
 
-    enum MotorConnector {
+    export enum MotorConnector {
         //%block="M1"
         M1 = 1,
         //%block="M2"
@@ -15,7 +15,7 @@ namespace mintspark {
         M4 = 4
     }
 
-    enum MotorMovementMode {
+    export enum MotorMovementMode {
         //%block="turns"
         Turns = 1,
         //%block="degrees"
@@ -24,7 +24,7 @@ namespace mintspark {
         Seconds = 3
     }
 
-    enum MotorRotationDirection {
+    export enum MotorRotationDirection {
         //%block="clockwise"
         CW = 1,
         //%block="counterclockwise"
@@ -205,10 +205,32 @@ namespace mintspark {
     //% weight=49
     //% subcategory="Motor / Servo"
     //% group="Motor Functions"
-    //%block="%NezhaV2MotorPostion speed (rpm)"
+    //%block="%motor speed (rpm)"
     //% color=#E63022
-    export function readServoAbsoluteSpeed(motor: NezhaV2MotorPostion): number {
-        return nezhaV2.readServoAbsoluteSpeed(motor) * 2;
+    export function readServoAbsoluteSpeed(motor: MotorConnector): number {
+        let buf = pins.createBuffer(8)
+        buf[0] = 0xFF;
+        buf[1] = 0xF9;
+        buf[2] = motor;
+        buf[3] = 0x00;
+        buf[4] = 0x47;
+        buf[5] = 0x00;
+        buf[6] = 0xF5;
+        buf[7] = 0x00;
+        pins.i2cWriteBuffer(i2cAddr, buf);
+        basic.pause(3);
+        let ServoSpeed1Arr = pins.i2cReadBuffer(i2cAddr, 2);
+        let Servo1Speed = (ServoSpeed1Arr[1] << 8) | (ServoSpeed1Arr[0]);
+        return Math.floor(Servo1Speed * 0.0926 * 2);
+    }
+
+    // A function to block the thread until a motor movement is completed or the max time has elapsed
+    function waitForMotorMovementComplete(motor: MotorConnector, maxTime: number): void {
+        basic.pause(100);
+        let startTime = input.runningTime();
+        while (readServoAbsoluteSpeed(motor) > 0 && (input.runningTime() - startTime) < maxTime) {
+            basic.pause(100);
+        }
     }
 
     //% weight=80
@@ -217,7 +239,7 @@ namespace mintspark {
     //% block="Set motor %motor to absolute angle %angle° direction %turnmode"
     //% color=#a3a3c2
     //% targetAngle.min=0  targetAngle.max=359
-    export function goToAbsolutePosition(motor: NezhaV2MotorPostion, targetAngle: number, turnMode: ServoMovementMode): void {
+    export function goToAbsolutePosition(motor: MotorConnector, targetAngle: number, turnMode: ServoMovementMode): void {
         while (targetAngle < 0) {
             targetAngle += 360
         }
@@ -234,23 +256,33 @@ namespace mintspark {
         buf[7] = (targetAngle >> 0) & 0XFF;
         pins.i2cWriteBuffer(i2cAddr, buf);
         
-        basic.pause(100);
         let maxTime = getMotorDelay(100, 1, MotorMovementMode.Turns);
-        let startTime = input.runningTime();
-
-        while (readServoAbsoluteSpeed(motor) > 0 && (input.runningTime() - startTime) < maxTime)
-        {
-            basic.pause(100);
-        }
+        waitForMotorMovementComplete(motor, maxTime);
     }
 
     //% weight=50
     //% subcategory="Motor / Servo"
     //% group="Servo Functions"
-    //%block="%NezhaV2MotorPostion absolute angular position"
+    //%block="%motor absolute angular position"
     //% color=#a3a3c2
-    export function readServoAbsolutePostion(motor: NezhaV2MotorPostion): number {
-        return nezhaV2.readServoAbsolutePostion(motor);
+    export function readServoAbsolutePostion(motor: MotorConnector): number {
+        let buf = pins.createBuffer(8);
+        buf[0] = 0xFF;
+        buf[1] = 0xF9;
+        buf[2] = motor;
+        buf[3] = 0x00;
+        buf[4] = 0x46;
+        buf[5] = 0x00;
+        buf[6] = 0xF5;
+        buf[7] = 0x00;
+        pins.i2cWriteBuffer(i2cAddr, buf);
+        basic.pause(4);
+        let arr = pins.i2cReadBuffer(i2cAddr, 4);
+        let position = (arr[3] << 24) | (arr[2] << 16) | (arr[1] << 8) | (arr[0]);
+        while (position < 0) {
+            position += 3600;
+        }
+        return (position % 3600) * 0.1;
     }
 
     /*
@@ -274,7 +306,7 @@ namespace mintspark {
     //% block="Set robot motor right to %motor reverse %reverse"
     //% subcategory="Robot Tank Mode"
     //% group="Setup"
-    //% motor.defl=NezhaV2MotorPostion.M1
+    //% motor.defl=MotorConnector.M1
     //% reverse.defl=false
     //% reverse.shadow="toggleYesNo"
     //% color=#E63022
@@ -287,7 +319,7 @@ namespace mintspark {
     //% block="Set robot motor left to %motor reverse %reverse"
     //% subcategory="Robot Tank Mode"
     //% group="Setup"
-    //% motor.defl=NezhaV2MotorPostion.M1
+    //% motor.defl=MotorConnector.M1
     //% reverse.defl=true
     //% reverse.shadow="toggleYesNo"
     //% color=#E63022
